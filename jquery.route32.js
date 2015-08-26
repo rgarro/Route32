74f30ff1fec1fd084980f4f38cbce663e2b9ba6a
Rolando Garro <rgarro@gmail.com>
2015-08-26 12:54:19 -0600

    Update jquery.route32.js
    license changes

--- jquery.route32.js
+++ jquery.route32.js
@@ -3,7 +3,25 @@
  * executes callback on location hash change that matches declared routes. 
  * Intenteded to be use as a piece on JavaScript MVC Apps 
  * 
- * Licensed under the MIT license.
+ * Copyright (C) 2010 by Rolando Garro
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
  * 
  * @author Rolando Garro <rgarro@gmail.com>
  * @requires jQuery
